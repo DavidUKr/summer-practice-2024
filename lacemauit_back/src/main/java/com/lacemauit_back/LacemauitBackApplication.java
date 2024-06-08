128ec7d8fb0504c2998ebc81dd19b25aa63a6d1e
DavidUKr <DavidUKr@users.noreply.github.com>
2024-06-08 10:47:12 +0300

    first commit

---
+++ lacemauit_back/src/main/java/com/lacemauit_back/LacemauitBackApplication.java
@@ -0,0 +1,13 @@
+package com.lacemauit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacemauitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacemauitBackApplication.class, args);
+	}
+
+}
